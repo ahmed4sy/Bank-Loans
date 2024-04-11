69aeb065b2806330e6a73fd29e6f0b1b75ab0873
ahmed4sy <hjlook99@gmail.com>
2024-04-11 11:14:32 +0300

    teht

--- src/Componets/DataIn.js
+++ src/Componets/DataIn.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "./../Styles/App.css";
+
 export default function DataIn({ Disp }) {
+  let contin = false;
+  let [Door, setDoor] = useState([false, false, false]);
   if (Disp[0] === "show") {
     return <StateStart />;
   } else if (Disp[1] === "show") {
-    return <StateForm />;
+    return <StateForm rec={Door} seting={setDoor} />;
   } else if (Disp[2] === "show") {
-    return <Done />;
+    contin = true;
+    return (
+      <Done
+        sneding={
+          Door[0] === true && Door[1] === true && Door[2] === true
+            ? true
+            : false
+        }
+      />
+    );
+  }
+  if (contin) {
+    setDoor([false, false, false]);
   }
 }
 
@@ -20,15 +36,80 @@ function StateStart() {
     </div>
   );
 }
-function StateForm() {
+function StateForm({ rec, seting }) {
+  let [Form, setForm] = useState(["", "", ""]);
   return (
     <div id="form">
       <h1>Request to Loan</h1>
-      <input type="text" className="name" placeholder="User Name.." />
-      <input type="text" className="age" placeholder="Age.." />
+      <input
+        type="text"
+        className="name"
+        value={Form[0]}
+        onChange={(e) => {
+          let newarr = [...Form];
+          if (e.target.value.length <= 25) {
+            newarr[0] = e.target.value;
+          }
+          if (e.target.value.length >= 4) {
+            let setForm = [...rec];
+            setForm[0] = true;
+            seting(setForm);
+          } else {
+            let setForm = [...rec];
+            setForm[0] = false;
+            seting(setForm);
+          }
+          setForm(newarr);
+        }}
+        placeholder="User Name.."
+      />
+      <input
+        type="text"
+        className="age"
+        value={Form[1]}
+        placeholder="Age.."
+        onChange={(e) => {
+          let newarr = [...Form];
+          if (e.target.value.length <= 2 && !isNaN(e.target.value)) {
+            newarr[1] = e.target.value;
+          }
+          if (Number(e.target.value) >= 18) {
+            let setForm = [...rec];
+            setForm[1] = true;
+            seting(setForm);
+          } else {
+            let setForm = [...rec];
+            setForm[1] = false;
+            seting(setForm);
+          }
+          setForm(newarr);
+        }}
+      />
 
       <div className="chi">
-        <input type="text" className="phone" placeholder="My Phone" />
+        <input
+          type="text"
+          className="phone"
+          value={Form[2]}
+          placeholder="My Phone"
+          onChange={(e) => {
+            let newarr = [...Form];
+            if (e.target.value.length <= 10 && !isNaN(e.target.value)) {
+              newarr[2] = e.target.value;
+            }
+            if (e.target.value.length === 10) {
+              let setForm = [...rec];
+              setForm[2] = true;
+              seting(setForm);
+            } else {
+              let setForm = [...rec];
+              setForm[2] = false;
+              seting(setForm);
+            }
+
+            setForm(newarr);
+          }}
+        />
         <input type="checkbox" className="check" />
         <p>Are you employed</p>
       </div>
@@ -41,8 +122,8 @@ function StateForm() {
     </div>
   );
 }
-function Done() {
-  if (false) {
+function Done({ sneding }) {
+  if (sneding) {
     return <h1 className="true">Sent Successfully</h1>;
   } else {
     return <h1 className="false">Sent Failed</h1>;
